b5f7db4748f4a17ab91870216ff9d60c89138e8c
rememberber <rememberber@163.com>
2019-08-17 11:01:33 +0800

    随手记支持设置字体

--- src/main/java/com/luoboduner/moo/tool/ui/form/fun/QuickNoteForm.java
+++ src/main/java/com/luoboduner/moo/tool/ui/form/fun/QuickNoteForm.java
@@ -130,6 +130,7 @@ public class QuickNoteForm {
         quickNotePanel.add(splitPane, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, new Dimension(200, 200), null, 0, false));
         final JPanel panel1 = new JPanel();
         panel1.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
+        panel1.setMinimumSize(new Dimension(0, 64));
         splitPane.setLeftComponent(panel1);
         final JPanel panel2 = new JPanel();
         panel2.setLayout(new GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1));
