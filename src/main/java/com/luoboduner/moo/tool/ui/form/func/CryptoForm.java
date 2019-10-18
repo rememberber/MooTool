0ff5a7915b497ddaceebd44c8c73a231d25bc136
duoduo <rememberber@163.com>
2019-10-18 16:35:52 +0800

    feature:增加对称加密界面

--- src/main/java/com/luoboduner/moo/tool/ui/form/func/CryptoForm.java
+++ src/main/java/com/luoboduner/moo/tool/ui/form/func/CryptoForm.java
@@ -2,6 +2,7 @@ package com.luoboduner.moo.tool.ui.form.func;
 
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
 import com.luoboduner.moo.tool.util.UndoUtil;
 import lombok.Getter;
 
@@ -20,6 +21,12 @@ import java.awt.*;
 public class CryptoForm {
     private JTabbedPane tabbedPane1;
     private JPanel cryptoPanel;
+    private JTextArea symTextAreaLeft;
+    private JTextArea symTextAreaRight;
+    private JButton symEncryptButton;
+    private JButton symDecryptButton;
+    private JTextField symKeyTextField;
+    private JComboBox symTypeComboBox;
 
     private static CryptoForm cryptoForm;
 
@@ -60,8 +67,39 @@ public class CryptoForm {
         tabbedPane1 = new JTabbedPane();
         cryptoPanel.add(tabbedPane1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, new Dimension(200, 200), null, 0, false));
         final JPanel panel1 = new JPanel();
-        panel1.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        panel1.setLayout(new GridLayoutManager(6, 4, new Insets(0, 0, 0, 0), -1, -1));
         tabbedPane1.addTab("对称加密（symmetric）", panel1);
+        symEncryptButton = new JButton();
+        symEncryptButton.setIcon(new ImageIcon(getClass().getResource("/icon/arrow-right.png")));
+        symEncryptButton.setText("加密");
+        panel1.add(symEncryptButton, new GridConstraints(3, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer1 = new Spacer();
+        panel1.add(spacer1, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        final Spacer spacer2 = new Spacer();
+        panel1.add(spacer2, new GridConstraints(5, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        symDecryptButton = new JButton();
+        symDecryptButton.setIcon(new ImageIcon(getClass().getResource("/icon/arrow-left.png")));
+        symDecryptButton.setText("解密");
+        panel1.add(symDecryptButton, new GridConstraints(4, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        symKeyTextField = new JTextField();
+        panel1.add(symKeyTextField, new GridConstraints(1, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        symTypeComboBox = new JComboBox();
+        final DefaultComboBoxModel defaultComboBoxModel1 = new DefaultComboBoxModel();
+        defaultComboBoxModel1.addElement("AES");
+        defaultComboBoxModel1.addElement("DES");
+        symTypeComboBox.setModel(defaultComboBoxModel1);
+        panel1.add(symTypeComboBox, new GridConstraints(2, 1, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label1 = new JLabel();
+        label1.setText("密钥");
+        panel1.add(label1, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JScrollPane scrollPane1 = new JScrollPane();
+        panel1.add(scrollPane1, new GridConstraints(0, 0, 6, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        symTextAreaLeft = new JTextArea();
+        scrollPane1.setViewportView(symTextAreaLeft);
+        final JScrollPane scrollPane2 = new JScrollPane();
+        panel1.add(scrollPane2, new GridConstraints(0, 3, 6, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        symTextAreaRight = new JTextArea();
+        scrollPane2.setViewportView(symTextAreaRight);
         final JPanel panel2 = new JPanel();
         panel2.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
         tabbedPane1.addTab("非对称加密（asymmetric）", panel2);
--- src/main/java/com/luoboduner/moo/tool/ui/listener/func/EnCodeListener.java
+++ src/main/java/com/luoboduner/moo/tool/ui/listener/func/EnCodeListener.java
@@ -16,26 +16,30 @@ public class EnCodeListener {
     public static void addListeners() {
         EnCodeForm enCodeForm = EnCodeForm.getInstance();
         enCodeForm.getNativeToUnicodeButton().addActionListener(e -> {
-            String nativeText1 = enCodeForm.getNativeTextArea1().getText();
+            String nativeText1 = enCodeForm.getNativeTextArea().getText();
             String unicodeText = UnicodeUtil.toUnicode(nativeText1);
             enCodeForm.getUnicodeTextArea().setText(unicodeText);
+            enCodeForm.getUnicodeTextArea().grabFocus();
         });
         enCodeForm.getUnicodeToNativeButton().addActionListener(e -> {
             String unicodeText = enCodeForm.getUnicodeTextArea().getText();
             String nativeText1 = UnicodeUtil.toString(unicodeText);
-            enCodeForm.getNativeTextArea1().setText(nativeText1);
+            enCodeForm.getNativeTextArea().setText(nativeText1);
+            enCodeForm.getNativeTextArea().grabFocus();
         });
         enCodeForm.getUrlEncodeButton().addActionListener(e -> {
             String url = enCodeForm.getUrlTextArea().getText();
             String urlEncodeCharset = (String) enCodeForm.getUrlEncodeCharsetComboBox().getSelectedItem();
             String urlEncode = URLUtil.encode(url, urlEncodeCharset);
             enCodeForm.getUrlEncodeTextArea().setText(urlEncode);
+            enCodeForm.getUrlEncodeTextArea().grabFocus();
         });
         enCodeForm.getUrlDecodeButton().addActionListener(e -> {
             String urlEncode = enCodeForm.getUrlEncodeTextArea().getText();
             String urlEncodeCharset = (String) enCodeForm.getUrlEncodeCharsetComboBox().getSelectedItem();
             String urlDecode = URLUtil.decode(urlEncode, urlEncodeCharset);
             enCodeForm.getUrlTextArea().setText(urlDecode);
+            enCodeForm.getUrlTextArea().grabFocus();
         });
     }
 }
