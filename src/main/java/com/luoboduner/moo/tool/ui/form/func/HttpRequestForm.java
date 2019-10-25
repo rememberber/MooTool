ff134803f32f0192b3814671b100e8f485a0496a
duoduo <rememberber@163.com>
2019-10-25 16:05:03 +0800

    optimization:优化发送http请求按钮图标

--- src/main/java/com/luoboduner/moo/tool/ui/form/func/HttpRequestForm.java
+++ src/main/java/com/luoboduner/moo/tool/ui/form/func/HttpRequestForm.java
@@ -507,8 +507,9 @@ public class HttpRequestForm {
         addButton.setToolTipText("新建");
         panel3.add(addButton, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         sendButton = new JButton();
-        sendButton.setIcon(new ImageIcon(getClass().getResource("/icon/sendToTheRight.png")));
+        sendButton.setIcon(new ImageIcon(getClass().getResource("/icon/send.png")));
         sendButton.setText("");
+        sendButton.setToolTipText("发送请求");
         panel3.add(sendButton, new GridConstraints(0, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JPanel panel4 = new JPanel();
         panel4.setLayout(new GridLayoutManager(2, 1, new Insets(8, 10, 10, 10), -1, -1));
