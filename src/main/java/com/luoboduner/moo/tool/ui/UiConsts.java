1270169d985f87dd4c72fcf505eab881edb3250f
duoduo <rememberber@163.com>
2019-11-03 16:37:07 +0800

    release:v1.2.0

--- src/main/java/com/luoboduner/moo/tool/ui/UiConsts.java
+++ src/main/java/com/luoboduner/moo/tool/ui/UiConsts.java
@@ -89,7 +89,7 @@ public class UiConsts {
     /**
      * 软件版本检查url
      */
-    public static final String CHECK_VERSION_URL = "https://raw.githubusercontent.com/rememberber/MooTool/master/src/main/resources/version_summary.json";
+    public static final String CHECK_VERSION_URL = "https://gitee.com/zhoubochina/MooTool/raw/master/src/main/resources/version_summary.json";
 
     /**
      * 二维码url
