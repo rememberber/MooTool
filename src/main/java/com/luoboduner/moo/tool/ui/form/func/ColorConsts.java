f225cdb6a9b400f6a3d80b837187064d9ffea07b
sunsence <sennybob@163.com>
2019-11-24 22:56:50 +0800

    feature:调色板增加颜色主题

--- src/main/java/com/luoboduner/moo/tool/ui/form/func/ColorConsts.java
+++ src/main/java/com/luoboduner/moo/tool/ui/form/func/ColorConsts.java
@@ -8,21 +8,21 @@ package com.luoboduner.moo.tool.ui.form.func;
  * @author <a href="https://github.com/rememberber">RememBerBer</a>
  * @since 2019/11/18.
  */
-public class ColorConsts {
+class ColorConsts {
 
     /**
      * 标准颜色
      */
-    public static final String[] STANDARD = {"C00000", "FF0000", "FFC000", "FFFF00", "92D050", "00B050", "00B0F0", "0070C0", "002060", "7030A0"};
+    static final String[] STANDARD = {"C00000", "FF0000", "FFC000", "FFFF00", "92D050", "00B050", "00B0F0", "0070C0", "002060", "7030A0"};
 
     /**
      * 主题-默认-main
      */
-    public static final String[] THEME_DEFAULT_MAIN = {"000000", "FFFFFF", "880015", "ED1C24", "FF7F27", "FFF200", "22B14C", "00A2E8", "3F48CC", "A349A4"};
+    static final String[] THEME_DEFAULT_MAIN = {"000000", "FFFFFF", "880015", "ED1C24", "FF7F27", "FFF200", "22B14C", "00A2E8", "3F48CC", "A349A4"};
     /**
      * 主题-默认-sub
      */
-    public static final String[][] THEME_DEFAULT_SUB = {{"808080", "595959", "404040", "262626", "0D0D0D"}, {"F2F2F2", "D9D9D9", "BFBFBF", "A6A6A6", "808080"},
+    static final String[][] THEME_DEFAULT_SUB = {{"808080", "595959", "404040", "262626", "0D0D0D"}, {"F2F2F2", "D9D9D9", "BFBFBF", "A6A6A6", "808080"},
             {"E7B9C0", "CF7C89", "B84A5B", "660010", "44000A"}, {"FBCFD0", "F8A1A4", "F47378", "B21016", "77070B"},
             {"FFE5D4", "FFCCA9", "FFB27D", "BF5B16", "803A0A"}, {"FFFCCC", "FFFA99", "FFF766", "BFB500", "807900"},
             {"C8EFD4", "98E0AD", "6BD089", "138535", "085820"}, {"C8EBFA", "94D8F6", "60C5F1", "007AAE", "005174"},
@@ -31,11 +31,11 @@ public class ColorConsts {
     /**
      * 主题-1-main
      */
-    public static final String[] THEME_1_MAIN = {"FFFFFF", "000000", "1F497D", "EEECE1", "4F81BD", "C0504D", "9BBB59", "8064A2", "4BACC6", "F79646"};
+    static final String[] THEME_1_MAIN = {"FFFFFF", "000000", "1F497D", "EEECE1", "4F81BD", "C0504D", "9BBB59", "8064A2", "4BACC6", "F79646"};
     /**
      * 主题-1-sub
      */
-    public static final String[][] THEME_1_SUB = {{"F2F2F2", "D9D9D9", "BFBFBF", "A6A6A6", "808080"}, {"808080", "595959", "404040", "262626", "0D0D0D"},
+    static final String[][] THEME_1_SUB = {{"F2F2F2", "D9D9D9", "BFBFBF", "A6A6A6", "808080"}, {"808080", "595959", "404040", "262626", "0D0D0D"},
             {"C3D2E5", "8EA9CB", "6185B1", "11345E", "08203E"}, {"D6D1B6", "B2AA7E", "776D38", "3C350E", "181502"},
             {"D6E3F2", "B0C8E5", "8CAED7", "2D598E", "14365F"}, {"F2D6D5", "E6B0AF", "D98D8B", "902E2B", "601513"},
             {"E9F1D8", "D4E4B4", "C0D693", "6F8C32", "465D16"}, {"E2DAEC", "C8B9DA", "AE99C7", "553879", "321951"},
@@ -44,13 +44,52 @@ public class ColorConsts {
     /**
      * 主题-2-main
      */
-    public static final String[] THEME_2_MAIN = {"FFFFFF", "000000", "1F497D", "EEECE1", "4F81BD", "C0504D", "9BBB59", "8064A2", "4BACC6", "F79646"};
+    static final String[] THEME_2_MAIN = {"FFFFFF", "000000", "69676D", "C9C2D1", "CEB966", "9CB084", "6BB1C9", "6585CF", "7E6BC9", "A379BB"};
     /**
      * 主题-2-sub
      */
-    public static final String[][] THEME_2_SUB = {{"F2F2F2", "D9D9D9", "BFBFBF", "A6A6A6", "808080"}, {"808080", "595959", "404040", "262626", "0D0D0D"},
-            {"C3D2E5", "8EA9CB", "6185B1", "11345E", "08203E"}, {"D6D1B6", "B2AA7E", "776D38", "3C350E", "181502"},
-            {"D6E3F2", "B0C8E5", "8CAED7", "2D598E", "14365F"}, {"F2D6D5", "E6B0AF", "D98D8B", "902E2B", "601513"},
-            {"E9F1D8", "D4E4B4", "C0D693", "6F8C32", "465D16"}, {"E2DAEC", "C8B9DA", "AE99C7", "553879", "321951"},
-            {"D6EEF4", "AEDCE8", "8BCCDD", "2A7E95", "135263"}, {"FDE9D9", "FCD4B4", "FABF8F", "B96927", "7C4212"}};
+    static final String[][] THEME_2_SUB = {{"F2F2F2", "D9D9D9", "BFBFBF", "A6A6A6", "808080"}, {"808080", "595959", "404040", "262626", "0D0D0D"},
+            {"E0E0E2", "C2C1C5", "A3A1A7", "423A52", "231A36"}, {"AB9DBC", "846D9D", "4A3068", "1F0C34", "0B0215"},
+            {"F5F0DC", "EBE1BC", "E2D49E", "9B873A", "67571A"}, {"EAEFE3", "D5DFC9", "C2D0B1", "6A844A", "3F5821"},
+            {"DDEEF4", "BDDEE9", "A0CFDF", "3C8097", "1B5164"}, {"DCE3F5", "BCCAEC", "9DB2E2", "39569B", "193168"},
+            {"E2DDF4", "C6BDE9", "ADA0DF", "4F3C97", "291B64"}, {"EBE0F1", "D8C4E4", "C6A9D6", "72448C", "461E5D"}};
+
+    /**
+     * 主题-3-main
+     */
+    static final String[] THEME_3_MAIN = {"FFFFFF", "000000", "323232", "E3DED1", "F07F09", "9F2936", "1B587C", "4E8542", "604878", "C19859"};
+    /**
+     * 主题-3-sub
+     */
+    static final String[][] THEME_3_SUB = {{"F2F2F2", "D9D9D9", "BFBFBF", "A6A6A6", "808080"}, {"808080", "595959", "404040", "262626", "0D0D0D"},
+            {"D6D6D6", "ADADAD", "848484", "262626", "191919"}, {"CCC2A9", "AA9B75", "716034", "392D0D", "171102"},
+            {"FCE4CB", "F9CA99", "F6B168", "B45E05", "783E02"}, {"ECC9CD", "D999A0", "C56D77", "771722", "500A12"},
+            {"C1D8E5", "8BB3CB", "5D91B0", "0F405D", "07293E"}, {"D4E7D0", "ACCEA4", "89B67F", "306425", "1A4311"},
+            {"DBD2E4", "B9A9C9", "9984AE", "41285A", "27123C"}, {"F3E9D9", "E6D2B4", "DABE94", "916C32", "604316"}};
+
+    /**
+     * 主题-4-main
+     */
+    static final String[] THEME_4_MAIN = {"FFFFFF", "000000", "646B86", "C5D1D7", "D16349", "CCB400", "8CADAE", "8C7B70", "8FB08C", "D19049"};
+    /**
+     * 主题-4-sub
+     */
+    static final String[][] THEME_4_SUB = {{"F2F2F2", "D9D9D9", "BFBFBF", "A6A6A6", "808080"}, {"808080", "595959", "404040", "262626", "0D0D0D"},
+            {"DBDEE7", "BABECF", "9AA0B6", "394265", "192243"}, {"9FB6C1", "6F90A1", "31586C", "0C2836", "020F16"},
+            {"F6DCD6", "EDBBAF", "E39B8A", "9D3F29", "682312"}, {"F5EFC4", "EBE08D", "E0D05A", "998700", "665A00"},
+            {"E6EFEF", "CEDEDF", "B6CDCE", "4E8082", "235557"}, {"E8E2DF", "D1C7C0", "BAACA4", "69503F", "462D1C"},
+            {"E6EFE5", "CEDFCD", "B9D0B6", "53844F", "275823"}, {"F6E7D6", "EDD0AF", "E3B98A", "9D6629", "683F12"}};
+
+    /**
+     * 主题-5-main
+     */
+    static final String[] THEME_5_MAIN = {"FFFFFF", "000000", "464646", "DEF5FA", "2DA2BF", "DA1F28", "EB641B", "39639D", "474B78", "7D3C4A"};
+    /**
+     * 主题-5-sub
+     */
+    static final String[][] THEME_5_SUB = {{"F2F2F2", "D9D9D9", "BFBFBF", "A6A6A6", "808080"}, {"808080", "595959", "404040", "262626", "0D0D0D"},
+            {"DADADA", "B5B5B5", "909090", "353535", "232323"}, {"B4D9E1", "7DB0BB", "37717D", "0E353E", "021519"},
+            {"CDEBF2", "9FD7E5", "75C5D9", "19788F", "0B4F60"}, {"F8CDD0", "F09EA2", "E97177", "A31118", "6D080D"},
+            {"FBDECF", "F7BEA0", "F39F72", "B0480F", "752D07"}, {"CDDAEB", "A1B8D8", "7999C4", "204476", "0E294F"},
+            {"D1D3E4", "A8ABC9", "8387AE", "282C5A", "12153C"}, {"E5CDD2", "CBA1AA", "B17A86", "5E222F", "3E0F19"}};
 }
