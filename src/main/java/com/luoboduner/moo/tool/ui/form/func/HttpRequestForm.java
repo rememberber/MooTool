71a0bb9573b0effdd9d84645aa819ad53e194b66
RememBerBer <rememberber@163.com>
2021-12-10 23:36:59 +0800

    UI细节调整，优化Windows11下的表现

--- src/main/java/com/luoboduner/moo/tool/ui/form/func/HttpRequestForm.java
+++ src/main/java/com/luoboduner/moo/tool/ui/form/func/HttpRequestForm.java
@@ -474,7 +474,7 @@ public class HttpRequestForm {
         splitPane = new JSplitPane();
         splitPane.setContinuousLayout(true);
         splitPane.setDividerLocation(0);
-        splitPane.setDividerSize(2);
+        splitPane.setDividerSize(10);
         httpRequestPanel.add(splitPane, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, new Dimension(200, 200), null, 0, false));
         final JPanel panel1 = new JPanel();
         panel1.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
@@ -521,7 +521,7 @@ public class HttpRequestForm {
         contentPanel.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
         rightPanel.add(contentPanel, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         final JPanel panel2 = new JPanel();
-        panel2.setLayout(new GridLayoutManager(1, 3, new Insets(2, 8, 10, 0), -1, -1));
+        panel2.setLayout(new GridLayoutManager(1, 3, new Insets(0, 0, 10, 0), -1, -1));
         contentPanel.add(panel2, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         final JLabel label1 = new JLabel();
         label1.setText("URL");
@@ -567,7 +567,7 @@ public class HttpRequestForm {
         paramTable.setRowHeight(36);
         scrollPane2.setViewportView(paramTable);
         final JPanel panel5 = new JPanel();
-        panel5.setLayout(new GridLayoutManager(1, 1, new Insets(10, 10, 10, 10), -1, -1));
+        panel5.setLayout(new GridLayoutManager(1, 1, new Insets(10, 5, 0, 0), -1, -1));
         tabbedPane1.addTab("Headers", panel5);
         final JPanel panel6 = new JPanel();
         panel6.setLayout(new GridLayoutManager(3, 5, new Insets(5, 5, 0, 0), -1, -1));
@@ -592,7 +592,7 @@ public class HttpRequestForm {
         headerTable.setRowHeight(36);
         scrollPane3.setViewportView(headerTable);
         final JPanel panel7 = new JPanel();
-        panel7.setLayout(new GridLayoutManager(1, 1, new Insets(10, 10, 10, 10), -1, -1));
+        panel7.setLayout(new GridLayoutManager(1, 1, new Insets(10, 5, 0, 0), -1, -1));
         tabbedPane1.addTab("Cookies", panel7);
         final JPanel panel8 = new JPanel();
         panel8.setLayout(new GridLayoutManager(3, 6, new Insets(5, 5, 0, 0), -1, -1));
@@ -632,7 +632,7 @@ public class HttpRequestForm {
         cookieTable.setRowHeight(36);
         scrollPane4.setViewportView(cookieTable);
         final JPanel panel9 = new JPanel();
-        panel9.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        panel9.setLayout(new GridLayoutManager(1, 1, new Insets(10, 5, 0, 0), -1, -1));
         tabbedPane1.addTab("Body", panel9);
         final JPanel panel10 = new JPanel();
         panel10.setLayout(new GridLayoutManager(2, 2, new Insets(5, 0, 0, 0), -1, -1));
