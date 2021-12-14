fd0989d53b5bfebbcca395bba77d5b1d51c26159
RememBerBer <rememberber@163.com>
2021-12-14 23:07:27 +0800

    UI细节调整，优化Windows11下的表现

--- src/main/java/com/luoboduner/moo/tool/ui/form/func/FavoriteColorForm.java
+++ src/main/java/com/luoboduner/moo/tool/ui/form/func/FavoriteColorForm.java
@@ -437,11 +437,11 @@ public class FavoriteColorForm {
      */
     private void $$$setupUI$$$() {
         favoriteColorPanel = new JPanel();
-        favoriteColorPanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        favoriteColorPanel.setLayout(new GridLayoutManager(1, 1, new Insets(12, 12, 12, 12), -1, -1));
         splitPane = new JSplitPane();
         splitPane.setContinuousLayout(true);
         splitPane.setDividerLocation(204);
-        splitPane.setDividerSize(2);
+        splitPane.setDividerSize(10);
         favoriteColorPanel.add(splitPane, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, new Dimension(200, 200), null, 0, false));
         final JPanel panel1 = new JPanel();
         panel1.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
