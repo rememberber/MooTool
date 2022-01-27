cd6ca2e3628c3ab753d96a6e1917bfe52c2c4305
RememBerBer <rememberber@163.com>
2022-01-27 15:06:51 +0800

    随手记：调整布局，调整提示器逻辑

--- src/main/java/com/luoboduner/moo/tool/ui/form/func/QuickNoteForm.java
+++ src/main/java/com/luoboduner/moo/tool/ui/form/func/QuickNoteForm.java
@@ -520,7 +520,7 @@ public class QuickNoteForm {
         findMenuSeparator = new JSeparator();
         findMenuSeparatorPanel.add(findMenuSeparator, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         final JPanel panel2 = new JPanel();
-        panel2.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
+        panel2.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
         controlPanel.add(panel2, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, true));
         contentSplitPane = new JSplitPane();
         contentSplitPane.setContinuousLayout(true);
