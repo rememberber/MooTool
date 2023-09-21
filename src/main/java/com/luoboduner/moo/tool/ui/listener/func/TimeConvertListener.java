1f99a895468848ea247d1a55f9cc0cda6116387e
rememberber <rememberber@163.com>
2023-09-22 00:04:57 +0800

    增加时钟大屏

--- src/main/java/com/luoboduner/moo/tool/ui/listener/func/TimeConvertListener.java
+++ src/main/java/com/luoboduner/moo/tool/ui/listener/func/TimeConvertListener.java
@@ -5,6 +5,7 @@ import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
 import com.luoboduner.moo.tool.ui.form.func.TimeConvertForm;
+import com.luoboduner.moo.tool.ui.frame.ClockFrame;
 import com.luoboduner.moo.tool.util.AlertUtil;
 import com.luoboduner.moo.tool.util.ConsoleUtil;
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -125,6 +126,11 @@ public class TimeConvertListener {
             @Override
             public void keyTyped(KeyEvent arg0) {
             }
+        });
+
+        timeConvertForm.getClockButton().addActionListener(e -> {
+            ClockFrame clockFrame = new ClockFrame();
+            clockFrame.setVisible(true);
         });
     }
 
