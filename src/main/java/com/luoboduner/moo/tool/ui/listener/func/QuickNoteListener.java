527871c9dfede2bddc37b3baabb9bb35d84eee9c
RememBerBer <rememberber@163.com>
2023-12-20 18:25:27 +0800

    随手记：增加搜索框

--- src/main/java/com/luoboduner/moo/tool/ui/listener/func/QuickNoteListener.java
+++ src/main/java/com/luoboduner/moo/tool/ui/listener/func/QuickNoteListener.java
@@ -399,7 +399,7 @@ public class QuickNoteListener {
 
             @Override
             public void changedUpdate(DocumentEvent e) {
-                QuickNoteForm.initNoteListTable();
+//                QuickNoteForm.initNoteListTable();
             }
         });
     }
