a6e471174c8f33ff366fb5fa576e8525b6429259
602228512@qq.com <Szgj@299792458>
2024-05-23 22:03:25 +0800

    1.增加yml和properties两种格式文本转换功能 2.pom 文件里的插件节点增加<pluginManagement>，因为我这边启动会报错，加了就不会有。 3.另外建议增加IOC框架，更方便开发。

--- src/main/java/com/luoboduner/moo/tool/util/YmlAndPropUtil.java
+++ src/main/java/com/luoboduner/moo/tool/util/YmlAndPropUtil.java
@@ -1,5 +1,6 @@
 package com.luoboduner.moo.tool.util;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.exceptions.UtilException;
 import cn.hutool.core.lang.Assert;
@@ -37,6 +38,15 @@ public class YmlAndPropUtil {
                 system.user.offset= 200
                 system.user.sex=1
                 spring.launcher=appl.class
+                
+                tencent.0=1001
+                tencent.1=2001
+                tencent.2=3002
+                
+                baidu.list[0]=a001
+                baidu.list[1]=b001
+                baidu.list[2]=c001
+              
                 """;
 
 
@@ -94,14 +104,25 @@ public class YmlAndPropUtil {
     private static void parseKey(Map<String, Object> data, String key, String value) {
         // 去除前后空格
         value = StrUtil.trim(value);
+        key = StrUtil.trim(key);
 
         String[] parts = key.split("\\.");
         Map<String, Object> current = data;
+        Map<String, Object> beforeCurrent = null;
         for (int i = 0; i < parts.length - 1; i++) {
+            if ((i == parts.length - 2 || parts.length <= 2)
+                    && StrUtil.isNumeric(parts[parts.length - 1])) {
+                // 如果最后一个key是数字 则将其转成列表
+                addObj2MapList(current, parts[i], value);
+                return;
+            }
+
             String part = parts[i];
             if (!current.containsKey(part)) {
                 current.put(part, new HashMap<>());
             }
+
+            // 指针下移
             current = (Map<String, Object>) current.get(part);
         }
         String lastPart = parts[parts.length - 1];
@@ -110,17 +131,22 @@ public class YmlAndPropUtil {
             String listKey = lastPart.substring(0, lastPart.indexOf('['));
 
             // 将list类型key转成map
-            List<Map<Integer, String>> list = (List<Map<Integer, String>>) current.get(listKey);
-            if (list == null) {
-                list = new ArrayList<>();
-                current.put(listKey, list);
-            }
-            Map<Integer, String> map = new HashMap<>();
-            map.put(Convert.toInt(lastPart.substring(lastPart.indexOf('[') + 1, lastPart.indexOf(']'))), value);
-            list.add(map);
+            addObj2MapList(current, listKey, value);
         } else {
             current.put(lastPart, value);
         }
+    }
+
+    private static void addObj2MapList(Map<String, Object> map, String key, Object value) {
+        map.compute(key, (k, v) -> {
+            if (v == null) {
+                return CollUtil.newArrayList(value);
+            } else {
+                List<Object> list = Convert.toList(Object.class, v);
+                list.add(value);
+                return list;
+            }
+        });
     }
 
     /**
