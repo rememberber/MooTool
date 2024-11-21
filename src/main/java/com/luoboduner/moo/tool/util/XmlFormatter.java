2303bb5722ce1495b9e0a6b70d19c2b13b2f69a3
rememberber <rememberber@163.com>
2024-11-21 17:15:36 +0800

    XmlFormatter

---
+++ src/main/java/com/luoboduner/moo/tool/util/XmlFormatter.java
@@ -0,0 +1,43 @@
+package com.luoboduner.moo.tool.util;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+public class XmlFormatter {
+
+    public static String format(String input) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new InputSource(new StringReader(input)));
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+            StringWriter writer = new StringWriter();
+            transformer.transform(new DOMSource(document), new StreamResult(writer));
+            return writer.toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Error formatting XML", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        String input = "<root><child><subchild>value</subchild></child></root>";
+        String output = format(input);
+        System.out.println(output);
+    }
+}
