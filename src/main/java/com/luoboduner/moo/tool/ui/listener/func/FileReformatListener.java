25c933f279484415f3e9489b314b9b088ef02a60
rememberber <rememberber@163.com>
2024-11-25 18:52:48 +0800

    格式化Tab优化

--- src/main/java/com/luoboduner/moo/tool/ui/listener/func/FileReformatListener.java
+++ src/main/java/com/luoboduner/moo/tool/ui/listener/func/FileReformatListener.java
@@ -33,6 +33,10 @@ public class FileReformatListener {
                 FileReformattingForm.changeStringType();
             }
         });
+
+        fileReformattingForm.getFormatButton().addActionListener(e -> {
+            FileReformattingForm.format();
+        });
     }
 
     public static class ParameterBuilder {
